2dd8243c80d30eb78ef7acfcdfbf2c924b1a8ee9
Robbie Jaeger <jaeger.rob@gmail.com>
2017-04-20 15:28:02 -0600

    Add knex config file

---
+++ knexfile.js
@@ -0,0 +1,32 @@
+module.exports = {
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/ideabox_knex_api_test',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/test'
+    }
+  },
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/ideabox_knex_api_shows',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/production'
+    }
+  }
+};
